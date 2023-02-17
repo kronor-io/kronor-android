5d2c83f4ca0666189f451e69a08b6c3f91716bb2
pranaysashank <pranaysashank@gmail.com>
2023-02-17 16:58:42 +0530

    Refactor app to example-app.

---
+++ example-app/src/main/java/io/kronor/example/MainActivity2.kt
@@ -0,0 +1,34 @@
+package io.kronor.example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import io.kronor.example.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main2)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main2)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
---
+++ example-app/src/main/java/io/kronor/example/Second2Fragment.kt
@@ -0,0 +1,69 @@
+package io.kronor.example
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.fragment.app.setFragmentResultListener
+import androidx.navigation.fragment.findNavController
+import io.kronor.api.Environment
+import io.kronor.component.swish.GetSwishComponent
+import io.kronor.component.swish.SwishConfiguration
+import java.util.*
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class Second2Fragment : Fragment() {
+    private var sessionToken : String? by mutableStateOf (null)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        setFragmentResultListener("sessionKey") { _, bundle ->
+            sessionToken = bundle.getString("sessionToken")
+        }
+        return ComposeView(requireContext()).apply {
+            setContent {
+                if (sessionToken != null) {
+                    val swishConfiguration = SwishConfiguration(sessionToken = sessionToken!!,
+                        merchantLogo = R.drawable.boozt_logo,
+                        environment = Environment.Staging,
+                        appName = "kronor-android-test",
+                        appVersion = "0.1.0",
+                        locale = Locale("en_US"),
+                        redirectUrl = Uri.parse("kronor_test://"),
+                        onPaymentSuccess = {
+                            Log.d("Second2Fragment", "Payment id: $it")
+                            findNavController().navigate(R.id.action_Second2Fragment_to_First2Fragment)
+                        },
+                        onPaymentFailure = {
+                            findNavController().navigate(R.id.action_Second2Fragment_to_First2Fragment)
+                        })
+                    GetSwishComponent(LocalContext.current, swishConfiguration)
+                }
+            }
+
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
